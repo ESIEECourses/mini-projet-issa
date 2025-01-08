83322c908e6e7be53d530cd1cd2cdcaa1063c230
ismahamat <your-email@example.com>
2025-01-08 15:46:46 +0100

    Modification de structure du code

--- src/components/header.py
+++ src/components/header.py
@@ -1,31 +1,6 @@
 from dash import Dash, html
 import dash_bootstrap_components as dbc
-from .component1 import render_histogram_seism_by_country, render_histogram_seism_by_year, render_histogram_seism_combine, histogram_magnitude_bycountry, histogram_death_magnitude
-from .component2 import render_choropleth_map_seism
-from .footer import get_footer
 
-def makeNavBar():
-    navbar = dbc.NavbarSimple(
-        children=[
-            dbc.NavItem(dbc.NavLink("Home", href="#")),
-            dbc.NavItem(dbc.NavLink("Histogram", href="#")),
-            dbc.NavItem(dbc.NavLink("Map", href="#")),
-        ],
-        brand_href="./header",
-        color="black",
-        dark=True,
-        className="justify-content-center",
-        style={
-            "border-radius":"15px",
-            "height":"75px", 
-            "margin-top":"-5px",
-            "justify-content": "center",  # Centre les éléments horizontalement
-            "align-items": "center"
-                
-        },
-
-    )
-    return navbar
 
 def create_header():
     return html.Div(
--- src/pages/home.py
+++ src/pages/home.py
@@ -1,5 +1,14 @@
 from ..components.navbar import makeNavBar
 from ..components.footer import get_footer
+from ..components.header import create_header
+from ..components.component1 import (
+    render_histogram_seism_by_country,
+    render_histogram_seism_by_year,
+    render_histogram_seism_combine,
+    histogram_death_magnitude,
+    histogram_magnitude_bycountry,
+)
+from ..components.component2 import render_choropleth_map_seism
 from dash import html
 
 
@@ -7,13 +16,85 @@ def init_home():
 
     return html.Div(
         children=[
+            create_header(),
             makeNavBar(),
-            html.H1("Bonjour et bienvenue sur la page d'accueil !",style={"margin-bottom" : "50px"}),
             html.Br(),
-            html.H3("Voici ci-dessous les différentes visualisation des données : ", style={"margin-bottom":"60px"}),
-            html.Div(children=[
-                html.A(html.P("Voir les histogrammes cliquez ici")),
-            ],style={"margin":"7em", "border" : "2px solid black", "border-radius":"20px", "background-color":"black", "padding":"100px"}),
+            html.H3(
+                children="Hello dash", style={"textAlign": "center", "color": "black"}
+            ),
+            html.Br(),
+            html.H3(
+                children="How are you today ?",
+                style={"textAlign": "center", "color": "black"},
+            ),
+            html.Br(),
+            html.Div(
+                children=[
+                    render_histogram_seism_by_country(),
+                ],
+                style={
+                    "margin": "7em",
+                    "border": "2px solid black",
+                    "border-radius": "20px",
+                    "background-color": "black",
+                    "padding": "100px",
+                },
+            ),
+            html.Div(
+                children=[
+                    render_histogram_seism_by_year(),
+                ],
+                style={
+                    "margin": "7em",
+                    "border": "2px solid black",
+                    "border-radius": "20px",
+                    "background-color": "black",
+                    "padding": "100px",
+                },
+            ),
+            html.Div(
+                children=[
+                    render_histogram_seism_combine(),
+                ],
+                style={
+                    "margin": "7em",
+                    "border": "2px solid black",
+                    "border-radius": "20px",
+                    "background-color": "black",
+                    "padding": "100px",
+                },
+            ),
+            html.Div(
+                children=[histogram_magnitude_bycountry()],
+                style={
+                    "margin": "7em",
+                    "border": "2px solid black",
+                    "border-radius": "20px",
+                    "background-color": "black",
+                    "padding": "100px",
+                },
+            ),
+            html.Div(
+                children=[render_choropleth_map_seism()],
+                style={
+                    "margin": "7em",
+                    "border": "2px solid black",
+                    "border-radius": "20px",
+                    "background-color": "black",
+                    "padding": "100px",
+                },
+            ),
+            html.Div(
+                children=[histogram_death_magnitude()],
+                style={
+                    "margin": "7em",
+                    "border": "2px solid black",
+                    "border-radius": "20px",
+                    "background-color": "black",
+                    "padding": "100px",
+                },
+            ),
             get_footer(),
-        ]
+        ],
+        style={"background-color": "#ccc4c4"},
     )
